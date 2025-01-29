fb31aa67c8d7e5712238545d449ef20501c7b1d3
Parker Brandt <parker.brandt101@gmail.com>
2025-01-29 15:59:42 -0600

    Implemented add, insert, and resize methods

--- ArrayList/ArrayList.java
+++ ArrayList/ArrayList.java
@@ -50,14 +50,24 @@ public class ArrayList<DT> {
      */
 
     /**
-     * Doubles the array capacity
+     * Multiplies the array capacity and copies over previous array
+     * O(n) time
      */
     private void resize() {
-        
+        DT[] newArr = (DT[]) new Object[this.array.length * ARRAY_RESIZE_FACTOR];
+
+        // Copy old array to new array
+        for (int i = 0; i < this.array.length; i++) {
+            newArr[i] = this.array[i];
+        }
+
+        this.array = newArr;
     }
 
     /**
      * Adds an element to the end of the array
+     * O(1) time if not resizing, O(n) time if resizing
+     * Amortized Time = O(1) time
      * @param elem
      */
     public void add(DT elem) {
@@ -66,19 +76,35 @@ public class ArrayList<DT> {
             resize();
         }
 
+        this.array[size] = elem;
+        this.size++;
     }
 
     /**
-     * Adds an element to the array at index i
+     * Inserts an element into the array at index i
      * Will push all elements >= i to the "right"
+     * O(n) time
      * @param elem
      * @param i
      */
-    public void add(DT elem, int i) {
+    public void insert(DT elem, int i) {
         // Check if need to resize first
         if (this.size >= this.array.length) {
             resize();
         }
+
+        DT swap = this.array[0];
+        for (int j = 0; j < this.array.length; j++) {
+            if (j == i) {
+                swap = this.array[j];
+                this.array[j] = elem;
+            } else if (j > i) {
+                DT temp = this.array[j];
+                this.array[j] = swap;
+                swap = temp;
+            }
+        }
+        this.size++;
     }
 
     /**
