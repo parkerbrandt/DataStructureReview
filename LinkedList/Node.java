a93fae4ddc3fe0a74cf513664abe355f8f33dfbe
Parker Brandt <parker.brandt101@gmail.com>
2025-01-29 16:30:16 -0600

    Implemented Node class

--- LinkedList/Node.java
+++ LinkedList/Node.java
@@ -1,5 +1,35 @@
 package LinkedList;
 
+
+/**
+ * Class to represent each node in a LinkedList
+ * Has information on its own data and pointers to the previous and next nodes
+ */
 public class Node<DT> {
     
+    /*
+     * Properties
+     */
+    private DT data;
+    public Node<DT> prev;
+    public Node<DT> next;
+
+    
+    /*
+     * Constructors
+     */
+    public Node(DT data) {
+        // Initialize variables
+        this.data = data;
+        this.prev = null;
+        this.next = null;
+    }
+
+
+    /*
+     * Methods
+     */
+    public DT getData() {
+        return this.data;
+    }
 }
